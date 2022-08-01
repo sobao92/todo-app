3c08c71efe1a572761547aac459f570af9225383
sobao92 <2mn69o0@gmail.com>
2022-08-01 14:20:42 +0900

    editアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'tasks/index'
   root to: 'tasks#index'
   resources :users, only: [:edit, :update]
-  resources :tasks, only: [:index, :new, :create]
+  resources :tasks, only: [:index, :new, :create, :edit]
 end
